0e3fc3572f505e2501bea7090a7ab86b7fd7a1d4
jherson <rodriguezpaul98@gmail.com>
2024-02-08 19:40:15 -0400

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
